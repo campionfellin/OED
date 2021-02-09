65efde6b56ef3e00a9286ca23bcbfa6665e9a269
campionfellin <campionfellin@gmail.com>
2021-02-08 18:54:29 -0800

    more logs

--- src/server/test/db/loadFromCsvStreamTests.js
+++ src/server/test/db/loadFromCsvStreamTests.js
@@ -85,14 +85,15 @@ mocha.describe('Read Mamac log from a file: ', () => {
 		myReadableStreamBuffer.put(buffer);
 		myReadableStreamBuffer.stop();
 		try {
-			console.log(myReadableStreamBuffer)
 			await loadFromCsvStream(myReadableStreamBuffer, row => {
 				const readRate = parseInt(row[0]);
 				const endTimestamp = moment(row[1], 'MM/DD/YYYY HH:mm');
 				const startTimestamp = moment(row[1], 'MM/DD/YYYY HH:mm').subtract(60, 'minutes');
 
 				return new Reading(meter.id, readRate, startTimestamp, endTimestamp);
-			}, (readings, tx) => Reading.insertOrIgnoreAll(readings, tx), conn);
+			}, (readings, tx) => Reading.insertOrIgnoreAll(readings, tx),
+			conn
+			);
 		} catch (e) {
 			const count = await Reading.count(conn);
 			expect(count).to.equal(0);
